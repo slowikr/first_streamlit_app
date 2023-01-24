ac8815a451058799b04eaf787918489cfae7be04
slowikr <36824502+slowikr@users.noreply.github.com>
2023-01-24 22:52:33 +0100

    Update streamlit_app.py
    Next change

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,5 @@ streamlit.title("My Parents New healthy Diner")
 ##Comment new update
 streamlit.header("Breakfast Menu")
 streamlit.text("Omega 3 & Blueberry Oatmeal")
+streamlit.text("Kale, Spinach & Rocket Smoothie")
+streamlit.text("Hard-Boiled Free-Range Egg")
