c259085c5ac09c7779ce5f88fc7a1e9cdcdbdad4
Radoslaw Slowik <radek.slowik@gmail.com>
2023-01-24 22:57:14 +0100

    Next Emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,6 @@ streamlit.title("My Parents New healthy Diner")
 ##Comment new update
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
