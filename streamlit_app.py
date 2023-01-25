3fe39ea3a75187c93a50e7a5ce5c7581517aa804
Radoslaw Slowik <radek.slowik@gmail.com>
2023-01-25 13:52:15 +0100

    Fruityvice

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,5 +25,7 @@ fruits_to_show=my_fruit_list.loc[my_fruit_list['Fruit'].apply(lambda x: x in fru
 #display the table on the page
 streamlit.dataframe(fruits_to_show)
 
+streamlit.header('Fruityvice Fruit Advice!')
+
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
