056012b97f60c671b5f74192271fa7d4832c2e71
Radoslaw Slowik <radek.slowik@gmail.com>
2023-01-25 15:47:52 +0100

    adding snowflake connection

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,3 +36,6 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_cho
 fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
 #output it the screen as a table
 streamlit.dataframe(fruityvice_normalized)
+
+#when using streamlit secrets then snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
